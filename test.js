1547d0802f9d73c2c981f6dc6fbd8eb19ea65ca4
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-10-29 21:47:56 +0530

    logic fixed and improved

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-const expression = 'A+B*C-D*E'
+var expression = 'A* B *C'
+expression = expression.split(' ').join('')
 var stack = []
 var convertedExpression = []
 var alphanumeric = /^[0-9a-zA-Z]+$/
@@ -12,27 +13,30 @@ const weightage = {
 }
 for (var i = 0; i < expression.length; i++) {
     var token = expression[i]
-    if (token.match(alphanumeric)) {
-        // if alphanumeric push into final array
+    if (token.match(alphanumeric)) { // if alphanumeric push into final array
         convertedExpression.push(token)
     } else {
-        // check weightage, push if more wightage than top
-        if (weightage[token] > weightage[stack[stack.length-1]] || stack.length === 0) {
+        if (weightage[token] >= weightage[stack[stack.length - 1]] || stack.length === 0 || token === '(') { // check weightage, push if more wightage than top
             stack.push(token)
-        } 
-        else {
+        } else if (token === ')') {
+            while (stack[stack.length - 1] != '(') {
+                el = stack.pop()
+                convertedExpression.push(el)
+            }
+            stack.pop()
+        } else {
             while (stack.length != 0) {
-                if (weightage[token] < weightage[stack[stack.length-1]]) {
-                    x = stack.pop()
-                    convertedExpression.push(x)
+                if (weightage[token] <= weightage[stack[stack.length - 1]]) {
+                    top = stack.pop()
+                    convertedExpression.push(top)
                 }
             }
             stack.push(token)
         }
     }
 }
-while(stack.length!=0){
-    x=stack.pop()
-    convertedExpression.push(x)
+while (stack.length != 0) {
+    top = stack.pop()
+    convertedExpression.push(top)
 }
-console.log(convertedExpression)
+console.log(convertedExpression.join(''))
