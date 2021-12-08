50a93880ce6f2540ff777e04e4a253467a7f9fe7
Arti <aartimanputra20@gmail.com>
2021-12-08 13:27:29 -0800

    Created a new component for Footer

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/navbar";
+import Footer from "./components/footer";
 
 const App=()=> {
   return (
   <div className="App">
      <NavBar/>
+     <Footer/>
   </div>
   );
 }
