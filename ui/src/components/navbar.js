bd424054fb879cf725df86c2bcbc7b8892dd7661
Manav Mohata <56617785+manav-06@users.noreply.github.com>
2021-12-09 20:10:01 +0530

    header fix

--- ui/src/components/navbar.js
+++ ui/src/components/navbar.js
@@ -3,15 +3,19 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import HomeIcon from '@mui/icons-material/Home';
-import { css } from '@emotion/react';
 import InfixToPostfix from "./infixtopostfix";
 import PostfixToInfix from "./postfixtoinfix";
 import InfixToPrefix from "./infixtoprefix";
 import PrefixToInfix from "./prefixtoinfix";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react'
+import Link from '@mui/material/Link';
+import HomeIcon from '@mui/icons-material/Home';
 
 const NavBar = () => {
 
@@ -22,34 +26,94 @@ const NavBar = () => {
     { title: `PrefixToInfix`, path: `/prefixtoinfix` }
   ]
 
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <>
-      <AppBar position="static" color="secondary" >
-        <Toolbar>
-          <IconButton edge="start" style={{ color: "yellow" }} aria-label="home">
-            <HomeIcon fontSize="large" />
-          </IconButton>
-          <Typography variant="h6" style={{ color: "yellow" }}>Notation-converter </Typography>
-          <List css={css`
-          color: 'primary',
-          display: 'flex',
-          justifyContent: 'space-between'
-        `}>
-            {navLinks.map(({ title, path }) => (
-              <a href={path} key={title} css={css`
-                  textDecoration: 'none',
-                  textTransform: 'uppercase',
-                  color: 'white'
-            `}>
-                <ListItem button>
-                  <ListItemText primary={title} />
-                </ListItem>
-              </a>
-            ))}
-          </List>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              <HomeIcon fontSize="large" />
+              Notation-Convertor
+            </Typography>
 
-        </Toolbar>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {navLinks.map(({ title, path }) => (
+                  <MenuItem key={title} onClick={handleCloseNavMenu}>
+                    <Link href={path} textAlign="center" underline='none'>{title}</Link>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              Notation-Convertor
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {navLinks.map(({ title, path }) => (
+                <Button
+                  key={title}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  href={path}
+                >
+                  {title}
+                </Button>
+              ))}
+            </Box>
+
+          </Toolbar>
+        </Container>
       </AppBar>
+
       <Router>
         <Routes>
           <Route path="/" element={<InfixToPostfix />} />
@@ -59,7 +123,9 @@ const NavBar = () => {
         </Routes>
       </Router>
     </>
-  )
+
+  );
+
 }
 
 export default NavBar;
