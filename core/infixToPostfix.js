1d651e412ce4be34f604c6cfe7c6875d21bdb124
Aditya Bisoi <adityabisoi1999@gmail.com>
2022-09-15 15:33:00 +0530

    conversion logic improved

--- core/infixToPostfix.js
+++ core/infixToPostfix.js
@@ -1,9 +1,10 @@
 function infixToPostfix(inputString) {
-  const expression = '(' + inputString.split(' ').join('') + ')';
+  const expression = inputString.split(' ').join('');
   var stack = [];
   var convertedExpression = [];
   const alphanumeric = /^[0-9a-zA-Z]+$/;
   const weightage = {
+    '^': 4,
     '*': 3,
     '/': 3,
     '+': 2,
@@ -11,31 +12,55 @@ function infixToPostfix(inputString) {
     '(': 0,
     ')': 0,
   };
+  var result = []
   for (var i = 0; i < expression.length; i++) {
     var token = expression[i];
+    var arr = []
+    var msg = ''
     if (token.match(alphanumeric)) {
+      msg = 'Add operand directly to output'
       convertedExpression.push(token);
-    } else {
-      if (weightage[token] === 0) {
-        if (token === '(') stack.push(token);
-        else {
-          while (stack[stack.length - 1] !== '(') {
-            top = stack.pop();
-            convertedExpression.push(top);
-          }
-          stack.pop();
-        }
-      } else {
-        while (weightage[token] <= weightage[stack[stack.length - 1]]) {
-          top = stack.pop();
-          convertedExpression.push(top);
-        }
-        stack.push(token);
+    } else if (token === '(') {
+      msg = 'Add ( to stack'
+      stack.push(token);
+    } else if (token === ')') {
+      msg = 'Pop operator(s) from stack and add to output until a ( is found'
+      while (stack[stack.length - 1] !== '(') {
+        top = stack.pop();
+        convertedExpression.push(top);
       }
+      stack.pop();
+    } else {
+      if (stack.length == 0) {
+        msg = 'Add operator to empty stack'
+      } else {
+        msg = 'Add operator to stack since last element in stack has lower precedence.'
+      }
+      while (weightage[token] <= weightage[stack[stack.length - 1]]) {
+        msg = 'Pop from stack and add to output, since the last operator in stack has higher/equal precedence'
+        top = stack.pop();
+        convertedExpression.push(top);
+      }
+      stack.push(token);
     }
+    arr.push(token)
+    arr.push(stack.join(''))
+    arr.push(convertedExpression.join(''))
+    arr.push(msg)
+    result.push(arr)
+  }
+  if ((stack.length != 0)) {
+    while (stack.length != 0) {
+      top = stack.pop();
+      convertedExpression.push(top);
+    }
+    result.push(['', '', convertedExpression.join(''), 'Pop remaining operators from stack and add to output'])
   }
   var ans = convertedExpression.join('');
-  return ans;
+  return  {
+    data:result,
+    output:ans
+  };
 }
 module.exports = {
   infixToPostfix: infixToPostfix,
--- core/infixToPrefix.js
+++ core/infixToPrefix.js
@@ -1,42 +1,73 @@
 function infixToPrefix(inputString) {
-  const expression = '(' + inputString.split(' ').join('') + ')';
+  const expression = inputString.split('').reverse().join('');
   var stack = [];
   var convertedExpression = [];
   const alphanumeric = /^[0-9a-zA-Z]+$/
   const weightage = {
-    '*': 3,
-    '/': 3,
-    '+': 2,
-    '-': 2,
-    '(': 0,
-    ')': 0,
+      '^': 4,
+      '*': 3,
+      '/': 3,
+      '+': 2,
+      '-': 2,
+      '(': 0,
+      ')': 0,
   };
-  for (var i = expression.length - 1; i >= 0; i--) {
-    var token = expression[i];
-    if (token.match(alphanumeric)) {
-      convertedExpression.push(token);
-    } else {
-      if (weightage[token] === 0) {
-        if (token === ')') stack.push(token);
-        else {
+  var result = []
+  result.push(['', '', '', `Reverse the infix expression: ${expression}`])
+  for (var i = 0; i < expression.length; i++) {
+      var token = expression[i];
+      var arr = []
+      var msg = ''
+      if (token == ' ') {
+          continue
+      }
+      if (token.match(alphanumeric)) {
+          msg = 'Add operand directly to output'
+          convertedExpression.push(token);
+      } else if (token === ')') {
+          msg = 'Add ) to stack'
+          stack.push(token);
+      } else if (token === '(') {
+          msg = 'Pop operator(s) from stack and add to output until a ) is found'
           while (stack[stack.length - 1] !== ')') {
-            top = stack.pop();
-            convertedExpression.push(top);
+              top = stack.pop();
+              convertedExpression.push(top);
           }
           stack.pop();
-        }
       } else {
-        while (weightage[token] < weightage[stack[stack.length - 1]]) {
+          if (stack.length == 0) {
+              msg = 'Add operator to empty stack'
+          } else {
+              msg = 'Add operator to stack since last element in stack has lower/equal precedence.'
+          }
+          while (weightage[token] < weightage[stack[stack.length - 1]]) {
+              msg = 'Pop from stack and add to output, since the last operator in stack has higher precedence'
+              top = stack.pop();
+              convertedExpression.push(top);
+          }
+          stack.push(token);
+      }
+      arr.push(token)
+      arr.push(stack.join(''))
+      arr.push(convertedExpression.join(''))
+      arr.push(msg)
+      result.push(arr)
+  }
+  if ((stack.length != 0)) {
+      while (stack.length != 0) {
           top = stack.pop();
           convertedExpression.push(top);
-        }
-        stack.push(token);
       }
-    }
+      result.push(['', '', convertedExpression.join(''), 'Pop remaining operators from stack and add to output'])
   }
   var ans = convertedExpression.reverse().join('');
-  return ans;
+  result.push(['', '', ans, 'Reverse the output'])
+  return {
+    data:result,
+    output:ans
+  };
 }
+
 module.exports = {
   infixToPrefix: infixToPrefix,
 };
